c8d8d340a95ec5c133d02d22cdf68db59d998d17
Jonathan <jgibler@gmail.com>
2016-04-10 14:08:53 +0100

    packages

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/public/index-video.html');
 });
 
 //start
